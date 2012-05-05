0c6a548f73ecffdaf088e11994030796e84bea07
hofmockel <dreagonfly@gmx.de>
2012-05-05 17:13:13 +0200

    Replate javascript hover with css

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,4 @@
 $(document).ready(function() {
-    /* Hide all 2nd menu at after loading */
-    $("#menu .menu li ul").hide();
-    /* Hover for the 2nd menu level */
-    $("#menu .menu li").hover(
-        function() { $(this).find("ul").show(); },
-        function() { $(this).find("ul").hide(); }
-    );
-
     $('#gallery a').lightBox({
       fixedNavigation: true,
       imageLoading:  'lightbox/images/lightbox-ico-loading.gif',
